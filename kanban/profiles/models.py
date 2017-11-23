4b0b36d952c6fc04196756c52fadbe2859d600ce
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-11-22 19:17:35 -0600

    Added profile model

--- kanban/profiles/models.py
+++ kanban/profiles/models.py
@@ -1,3 +1,29 @@
+import settings
+
 from django.db import models
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Holds information about a user.
+    """
+
+    MALE_CHOICE = 1
+    FEMALE_CHOICE = 2
+    OTHER_CHOICE = 3
+
+    GENDER_CHOICES = (
+        (MALE_CHOICE, 'Male'),
+        (FEMALE_CHOICE, 'Female'),
+        (NOT_SPECIFIED_CHOICE, 'Not specified')
+    )
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images', null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, default=NOT_SPECIFIED_CHOICE)
+
+    def __str__(self):
+
+        return '%s\'s profile' % self.user.get_username()
