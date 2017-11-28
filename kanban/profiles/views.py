e8f87e7f69b2e730739a6b6d92bef36597dfef0b
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-11-27 20:13:34 -0600

    Added set password endpoint

--- kanban/profiles/views.py
+++ kanban/profiles/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
 
+from rest_framework import status
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from profiles.serializers import UserSerializer
+from profiles.serializers import UserSerializer, ChangePasswordSerializer
 
 
 USER = get_user_model()
@@ -16,5 +19,15 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
 
+    @detail_route(methods=['post'])
+    def set_password(self, request, pk=None):
+        user = self.get_object()
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user.set_password(serializer.data['new_password'])
+            user.save()
+            return Response({'status': 'password set'})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
