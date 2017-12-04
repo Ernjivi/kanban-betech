726c0d37427f9ebe95f15213d7205ede7e83bdde
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-12-04 13:19:14 -0600

    Added url for jwt auth

--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 from profiles.urls import urlpatterns as profiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', obtain_jwt_token)
 ] + profiles_urlpatterns
