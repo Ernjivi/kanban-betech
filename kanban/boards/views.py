5627b58d65a2d3f6eb3f64a5fff7feede48f0e4d
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2017-12-12 13:12:13 -0600

    Added new viewsets
    - Added CardViewSet
    - Added CheckListViewSet
    - Added CheckItemViewSet

--- kanban/boards/views.py
+++ kanban/boards/views.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
-from boards.models import Board
-from boards.serializers import BoardSerializer
+from boards.models import Board, Card, CheckList, CheckItem
+from boards.serializers import (
+    BoardSerializer,
+    CardSerializer,
+    CheckListSerializer,
+    CheckItemSerializer,
+)
 
 
 class BoardViewSet(ModelViewSet):
@@ -11,3 +16,30 @@ class BoardViewSet(ModelViewSet):
 
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+
+class CardViewSet(ModelViewSet):
+    """
+    List, retieve, add, update instances of Card model.
+    """
+
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class CheckListViewSet(ModelViewSet):
+    """
+    List, retrieve, add, update instances of CheckList model.
+    """
+
+    queryset = CheckList.objects.all()
+    serializer_class = CheckListSerializer
+
+
+class CheckItemViewSet(ModelViewSet):
+    """
+    List, retriebe, add, update instances of CheckItem model.
+    """
+
+    queryset = CheckItem
+    serializer_class = CheckItemSerializer
